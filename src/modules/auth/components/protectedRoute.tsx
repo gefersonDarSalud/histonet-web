0eff872190fe762463f07883542cf734ffaa1bc8
Geferson Moreno <geferson.moreno@darsalud.com.ve>
2025-12-22 15:14:01 -0400

    refactor: replace custom loading spinner with LoadingCircle component in ProtectedRoute

--- src/modules/auth/components/protectedRoute.tsx
+++ src/modules/auth/components/protectedRoute.tsx
@@ -2,6 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import type { ReactElement } from "react";
 import { routeLabel } from "#/routes";
+import { LoadingCircle } from "@/components/app/loading";
 
 export const ProtectedRoute = ({ children }: { children: ReactElement; }) => {
     const { isLoggedIn, isAuthReady } = useAuth();
@@ -10,16 +11,14 @@ export const ProtectedRoute = ({ children }: { children: ReactElement; }) => {
     if (!isAuthReady) {
         return (
             <div className="flex justify-center items-center h-screen bg-gray-100">
-                <svg className="animate-spin h-8 w-8 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
+                <LoadingCircle />
             </div>
         );
     }
 
     const toMain = () => <Navigate to={routeLabel.login} replace state={{ from: location }} />
 
+    if (location.pathname === "/") return toMain();
     if (!isLoggedIn) return toMain();
 
     return children;
